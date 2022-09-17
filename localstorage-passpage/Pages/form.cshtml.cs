1afa588d90c0aee8ef23f20e883f8a1b4619095c
stanleyshih <stanley155292@gmail.com>
2022-09-18 04:21:37 +0800

    first commit

---
+++ localstorage-passpage/Pages/form.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace localstorage_passpage.Pages
+{
+    public class formModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
